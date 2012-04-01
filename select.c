32daad3a3a60cb449aaeb4052cfc764af1743e32
Ji Zhu <jizhu1@linux5.ews.illinois.edu>
2012-04-01 17:45:39 -0500

    select

--- select.c
+++ select.c
@@ -7,28 +7,35 @@
 int main(void)
 {
     fd_set rfds;
-    struct timeval tv;
+    struct timeval tv,constv;
     int retval;
-
+    char buf[100];
    /* Watch stdin (fd 0) to see when it has input. */
     FD_ZERO(&rfds);
     FD_SET(0, &rfds);
 
    /* Wait up to 0.1 seconds. */
-    tv.tv_sec = 0.01;
-    tv.tv_usec = 0;
-    const int timeout = 100;
+    constv.tv_sec = 0;
+    constv.tv_usec = 10000;
+    tv = constv;
+    const int timeout = 50;
     int time = timeout;
     
     while(retval = select(1, &rfds, NULL, NULL, &tv)>=0){
-        time--;
+        printf("remain time: %.2f \n",(float)tv.tv_usec/1e6+tv.tv_sec);
+	time--;
         if(time==0){
             printf("timeout: do something here\n");
+	    time = timeout;
         }
-        if (retval) {
-            printf("Data is available now.\n");
+        if (FD_ISSET(0,&rfds)) {
+           	fgets(buf,100,stdin);
+		 printf("Data is available now: %s\n", buf);
         }
-        
+	tv = constv;
+	FD_ZERO(&rfds);
+	FD_SET(0,&rfds);
+       
     }
     /* Don't rely on the value of tv now! */
 
